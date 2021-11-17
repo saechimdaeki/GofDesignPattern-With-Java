2c3256f54c430c7d424194d1fa918bebae37d26b
saechim <anima94@kakao.com>
2021-11-17 22:03:44 +0900

    책임 연쇄 패턴

---
+++ src/main/java/com/example/gofdesignpatternwithjava/_03_behavioral_patterns/_13_chain_of_responsibilities/_01_before/Request.java
@@ -0,0 +1,11 @@
+package com.example.gofdesignpatternwithjava._03_behavioral_patterns._13_chain_of_responsibilities._01_before;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter @Setter
+public class Request {
+    private String body;
+}
---
+++ src/main/java/com/example/gofdesignpatternwithjava/_03_behavioral_patterns/_13_chain_of_responsibilities/_02_after/PrintRequestHandler.java
@@ -0,0 +1,17 @@
+package com.example.gofdesignpatternwithjava._03_behavioral_patterns._13_chain_of_responsibilities._02_after;
+
+import com.example.gofdesignpatternwithjava._03_behavioral_patterns._13_chain_of_responsibilities._01_before.Request;
+
+public class PrintRequestHandler extends RequestHandler {
+
+
+    public PrintRequestHandler(RequestHandler nextHandler) {
+        super(nextHandler);
+    }
+
+    @Override
+    public void handle(Request request) {
+        System.out.println(request.getBody());
+        super.handle(request);
+    }
+}
