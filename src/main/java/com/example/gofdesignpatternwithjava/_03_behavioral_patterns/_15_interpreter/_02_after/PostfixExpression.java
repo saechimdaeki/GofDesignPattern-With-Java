9b6fa7759a30f20835cb6ec9727850fa19bf3e40
saechim <anima94@kakao.com>
2021-11-18 21:42:00 +0900

    인터프리터 패턴

---
+++ src/main/java/com/example/gofdesignpatternwithjava/_03_behavioral_patterns/_15_interpreter/_02_after/PostfixExpression.java
@@ -0,0 +1,7 @@
+package com.example.gofdesignpatternwithjava._03_behavioral_patterns._15_interpreter._02_after;
+
+import java.util.Map;
+
+public interface PostfixExpression {
+    int interpret(Map<Character,Integer> context);
+}
---
+++ src/main/java/com/example/gofdesignpatternwithjava/_03_behavioral_patterns/_15_interpreter/_02_after/VariableExpression.java
@@ -0,0 +1,17 @@
+package com.example.gofdesignpatternwithjava._03_behavioral_patterns._15_interpreter._02_after;
+
+import java.util.Map;
+
+public class VariableExpression implements PostfixExpression{
+
+    private Character variable;
+
+    public VariableExpression(Character variable) {
+        this.variable = variable;
+    }
+
+    @Override
+    public int interpret(Map<Character, Integer> context) {
+        return context.get(variable);
+    }
+}
