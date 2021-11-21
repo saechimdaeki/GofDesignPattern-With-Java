8e565d4693bcbc3afe7fc888ec0587e1395e98b2
saechim <anima94@kakao.com>
2021-11-21 13:45:38 +0900

    전략 패턴

---
+++ src/main/java/com/example/gofdesignpatternwithjava/_03_behavioral_patterns/_21_strategy/_02_after/Client.java
@@ -0,0 +1,21 @@
+package com.example.gofdesignpatternwithjava._03_behavioral_patterns._21_strategy._02_after;
+
+public class Client {
+
+    public static void main(String[] args) {
+        BlueLightRedLight game = new BlueLightRedLight();
+        game.blueLight(new Normal());
+        game.redLight(new Fastest());
+        game.blueLight(new Speed() {
+            @Override
+            public void blueLight() {
+                System.out.println("blue light");
+            }
+
+            @Override
+            public void redLight() {
+                System.out.println("red light");
+            }
+        });
+    }
+}
